ee5e083bf30b9b639881b0d957da0e2c88759085
Alberto Guerrero <alberto11160@gmail.com>
2024-10-26 12:24:33 +0200

    Commit 2024-10-26: Login/Logout logic

--- src/project/base/urls.py
+++ src/project/base/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PendingList, DetailTask, CreateTask, EditTask, DeleteTask
+from .views import PendingList, DetailTask, CreateTask, EditTask, DeleteTask, Log_in
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [path('', PendingList.as_view(), name='pending'),
+               path('login/', Log_in.as_view(), name='login'),
+               path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
                path('task/<int:pk>', DetailTask.as_view(), name='task'),
                path('create-task/', CreateTask.as_view(), name='create-task'),
                path('edit-task/<int:pk>', EditTask.as_view(), name='edit-task'),
-               path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task')]
+               path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task')
+               ]
--- src/project/base/views.py
+++ src/project/base/views.py
@@ -2,35 +2,46 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 
-class PendingList(ListView):
+class Log_in(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('pending')
+
+
+class PendingList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'base/task_list.html'
 
-class DetailTask(DetailView):
+class DetailTask(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/detail_task.html'
 
 
-class CreateTask(CreateView):
+class CreateTask(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('pending')
     # When we get a successful event it goes to the URL named "pending" check in urls.py
 
 
-class EditTask(UpdateView):
+class EditTask(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('pending')
 
 
-class DeleteTask(DeleteView):
+class DeleteTask(LoginRequiredMixin, DeleteView):
     model = Task
     fields = '__all__'
     template_name = 'base/task_deleted.html'
